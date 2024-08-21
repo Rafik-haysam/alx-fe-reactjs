d194a9d596d6fbebbe684019ee5585321b85d195
Rafik-haysam <rafik.creative@gmail.com>
2024-08-21 16:02:48 +0300

    update1

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
-import MainContent from '.src/components/MainContent'
-import Header from '.src/components/Header'
-import Footer from '.src/components/Footer'
+import MainContent from './components/MainContent'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 
