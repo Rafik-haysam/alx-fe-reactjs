bfdb771da48a2cfa6f1fae6e6ca8bf3a54c23650
Rafik haysam ahmed <157654881+Rafik-haysam@users.noreply.github.com>
2024-09-07 22:57:11 +0300

    Add files via upload

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,17 +1,16 @@
-// src/components/Counter.jsx
 import { useState } from 'react';
 
 function Counter() {
   const [count, setCount] = useState(0); // Initialize state with 0
 
-  return (
-    <div>
-      <p>Current Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
-    </div>
-  );
+return (
+<div>
+    <p>Current Count: {count}</p>
+    <button onClick={() => setCount(count + 1)}>Increment</button>
+    <button onClick={() => setCount(count - 1)}>Decrement</button>
+    <button onClick={() => setCount(0)}>Reset</button>
+</div>
+);
 }
 
 export default Counter;
